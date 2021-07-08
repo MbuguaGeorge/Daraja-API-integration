2ba08b4c88f46cb8ffe0e9febb1d77465d6c0453
MbuguaGeorge <mbuguag026@gmail.com>
2021-07-08 14:23:39 +0300

    API Authorization

--- trial.py
+++ trial.py
@@ -2,8 +2,8 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-consumer_key = "CONSUMER KEY"
-consumer_secret = "CONSUMER SECRET"
+consumer_key = "CONSUMER_KEY"
+consumer_secret = "CONSUMER_SECRET"
 api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
 
 r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
